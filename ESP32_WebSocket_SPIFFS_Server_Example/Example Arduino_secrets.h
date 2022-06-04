79b09c000805789c01ca291eb5cfaec7bd00710d
Haggarman <96515734+Haggarman@users.noreply.github.com>
2022-06-04 18:18:22 -0500

    Create Example Arduino_secrets.h
    Example Arduino_secrets.h

---
+++ ESP32_WebSocket_SPIFFS_Server_Example/Example Arduino_secrets.h
@@ -0,0 +1,3 @@
+//Edit this with your details and Save As "Arduino_secrets.h"
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
